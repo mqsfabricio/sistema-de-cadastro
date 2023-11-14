438f65120bd5783f8b3684a84778aa06cb22e026
Marcos Quaresma <marcos.quaresma@ciahering.com.br>
2023-11-14 10:21:01 -0300

    Mais um commit de teste

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao bando de dados")
+// mais um console log de teste:
+console.log("Mas uma alteração para fazer o commit")
+
