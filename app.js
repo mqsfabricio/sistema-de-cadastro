32c012c9420c5843a573235e1c4c8508aaea412f
Marcos Quaresma <marcos.quaresma@ciahering.com.br>
2023-11-14 13:52:28 -0300

    Corrigindo o app js

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao bando de dados")
 // mais um console log de teste:
 console.log("Mas uma alteração para fazer o commit")
-
