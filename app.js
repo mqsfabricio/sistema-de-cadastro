8dc6d3c7bcb716722e37ab995d6cf6444a9cb6e2
Marcos Quaresma <marcos.quaresma@ciahering.com.br>
2023-11-14 15:51:49 -0300

    Criação de tabela no banco de dados

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao bando de dados")
 // mais um console log de teste:
 console.log("Mas uma alteração para fazer o commit")
+console.log("Foi criado na aplicação a tabela JOGOS no banco de dados")
