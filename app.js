a168999b18135381d16d50e15ce6bf5810743220
Marcos Quaresma <marcos.quaresma@ciahering.com.br>
2023-11-14 11:02:22 -0300

    Voltando para o estado anterior

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao bando de dados")
 // mais um console log de teste:
 console.log("Mas uma alteração para fazer o commit")
-console.log("Enviando para o GitHub")
+
